9b3fac28eca85c60a2a3175cd65a947b6879a589
apetrycki <34962392+apetrycki@users.noreply.github.com>
2024-08-15 12:35:56 -0400

    await async_forward_entry_setups

--- custom_components/daikinskyport/__init__.py
+++ custom_components/daikinskyport/__init__.py
@@ -93,10 +93,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
-
 
     undo_listener = entry.add_update_listener(update_listener)
 
@@ -104,6 +100,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         COORDINATOR: coordinator,
         UNDO_UPDATE_LISTENER: undo_listener
     }
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
